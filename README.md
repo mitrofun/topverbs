697abfb52c9b4cb835ed6ebf1edd8c0e27649d7d
Dmitriy Shesterkin <mitri4@bk.ru>
2018-07-28 01:47:26 +0300

    add setup.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Counting the frequency of using verbs in function names
+[![Build Status](https://travis-ci.org/mitrofun/topverbs.svg?branch=master)](https://travis-ci.org/mitrofun/topverbs)
+
 Script to count the number of verbs used in function names.
---
+++ setup.py
@@ -0,0 +1,10 @@
+import topverbs
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='topverbs',
+    version=topverbs.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+)
