2eb59804c1947972c95d9dc1c3914bc314e10d08
Dmitriy Shesterkin <mitri4@bk.ru>
2018-08-02 10:53:41 +0300

    ignore setup.py in pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, ROOT_DIR)
 sys.path.insert(0, os.path.join(ROOT_DIR, 'topverbs'))
 
+collect_ignore = ["setup.py"]
+
 
 @pytest.fixture
 def root_path():
